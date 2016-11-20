dc2ca54d102e141b024645bd08afa3357d51886a
Josh Chisholm <joshuachisholm@gmail.com>
2016-11-20 12:23:56 +0000

    Rewrite stdout, exit codes

--- renderer.js
+++ renderer.js
@@ -1,3 +1,4 @@
+var electron = require('electron')
 var Convert = require('ansi-to-html')
 var convert = new Convert()
 var Cucumber = require('cucumber')
@@ -8,22 +9,32 @@ require('gherkin').Parser = window.Gherkin.Parser
 require('gherkin').Compiler = window.Gherkin.Compiler
 require('gherkin').DIALECTS = window.Gherkin.DIALECTS
 
-var Formatter = Cucumber.Listener.Formatter
-Cucumber.Listener.Formatter = function(options) {
-  options.logToFunction = function() {
-    var args = [].slice.apply(arguments)
-    var output = convert.toHtml(args[0].toString(), { fg: '#000', bg: '#fff' })
-    var pre = document.createElement('pre')
-    pre.innerHTML = output
-    document.body.appendChild(pre)
-  }
-  return Formatter(options)
+var stdout = electron.remote.process.stdout
+
+function log() {
+  var args = [].slice.apply(arguments)
+  stdout.write.apply(null, args)
+  var output = convert.toHtml(args[0].toString(), { fg: '#000', bg: '#fff' })
+  var pre = document.createElement('pre')
+  pre.innerHTML = output
+  document.body.appendChild(pre)
+}
+
+var PrettyFormatter = Cucumber.Listener.PrettyFormatter
+Cucumber.Listener.PrettyFormatter = function(options) {
+  options.logToFunction = log
+  return PrettyFormatter(options)
 }
 
 var args = JSON.parse(decodeURIComponent(window.location.hash.substr(1)))
 
 var cli = Cucumber.Cli([`${__dirname}/node_modules/.bin/cucumberjs`, __dirname].concat(args));
 
-cli.run(function (succeeded) {
-  // TODO: tell the parent process what happened so it can exit
-});
+try {
+  cli.run(function (succeeded) {
+    electron.remote.process.exit(succeeded ? 0 : 1)
+  });
+} catch (err) {
+  log(err.stack)
+  electron.remote.process.exit(2)
+}
