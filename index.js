95f780020473b6ac6f0424e0f73366e86628cdcb
Josh Chisholm <joshuachisholm@gmail.com>
2017-01-10 14:54:53 +0000

    Disable web security so we can load stuff in iframes

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    show: !!cli.electronDebug
+    show: !!cli.electronDebug,
+    webPreferences: { webSecurity: false }
   })
 
   mainWindow.loadURL(url.format({
