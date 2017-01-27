3e6d84d9e3869824e7ec73bccdee2c6446b88131
Josh Chisholm <joshuachisholm@gmail.com>
2017-01-27 16:12:29 +0000

    Remove which dependency for now

--- bin/cucumber-electron.js
+++ bin/cucumber-electron.js
@@ -2,35 +2,20 @@
 
 var spawn = require('child_process').spawn
 var path = require('path')
-var which = require('which')
 
-var electron = process.env.ELECTRON_PATH ||
-  resolve('electron') || resolve('electron-prebuilt') ||
-  resolve('electron', which.sync)
+var electron = require('electron')
 
-run(electron)
+var args = process.argv.slice(2)
+args.unshift(path.resolve(path.join(__dirname, '../index.js')))
 
-function resolve(module, resolver) {
-  try {
-    return (resolver || require)(module)
-  } catch (_) {
-    // ignore
-  }
-}
+var child = spawn(electron, args)
+child.stdout.pipe(process.stdout)
+process.stdin.pipe(child.stdin)
 
-function run(electron) {
-  var args = process.argv.slice(2)
-  args.unshift(path.resolve(path.join(__dirname, '../index.js')))
-
-  var child = spawn(electron, args)
-  child.stdout.pipe(process.stdout)
-  process.stdin.pipe(child.stdin)
-
-  child.stderr.on('data', function (data) {
-    var str = data.toString('utf8')
-    // it's Chromium, STFU
-    if (str.match(/^\[\d+\:\d+/)) return
-    process.stderr.write(data)
-  })
-  child.on('exit', function (code) { process.exit(code) })
-}
+child.stderr.on('data', function (data) {
+  var str = data.toString('utf8')
+  // Mute irrelevant chromium errors
+  if (str.match(/^\[\d+\:\d+/)) return
+  process.stderr.write(data)
+})
+child.on('exit', function (code) { process.exit(code) })
