876d7761a9933c8116871880c1f14f45052b88bd
Josh Chisholm <joshuachisholm@gmail.com>
2017-10-10 15:30:15 +0100

    Remove gherkin patch

--- renderer/patches/gherkin.js
+++
@@ -1,8 +0,0 @@
-// HACK:
-// window.Gherkin !== require('gherkin') in browsers
-// Until the fix is released, parsing fails unless we do this:
-
-const gherkin = require('gherkin')
-gherkin.Parser = window.Gherkin.Parser
-gherkin.Compiler = window.Gherkin.Compiler
-gherkin.DIALECTS = window.Gherkin.DIALECTS
