bd099ac54916bc738a0317c08092cf6ba894c96b
Josh Chisholm <joshuachisholm@gmail.com>
2018-02-20 17:09:10 +0000

    CLI --help, fixes #16

---
+++ cli/help.js
@@ -0,0 +1,23 @@
+const Cucumber = require('cucumber')
+
+const write = process.stdout.write
+process.stdout.write = (chunk, ...args) => {
+  chunk = chunk.split('\n').map(rewriteExitLine).join('\n')
+  write.call(process.stdout, chunk, ...args)
+}
+
+function rewriteExitLine(line) {
+  if (line.match(/\s+--exit/) == null) {
+    return line
+  }
+  return [
+    '    --exit                          UNSUPPORTED in cucumber-electron: force shutdown of the event loop when the test run has finished',
+    '    -i, --interactive               open an interactive debugger (chromium dev tools)'
+  ].join('\n')
+}
+
+new Cucumber.Cli({
+  argv: ['node', 'cucumber-electron', '--help'],
+  cwd: process.cwd(),
+  stdout: process.stdout
+}).run()
---
+++ features/help.feature
@@ -0,0 +1,7 @@
+Feature: Help
+  Scenario: Running the CLI with --help
+    When I run `cucumber-electron --help`
+    Then stdout should include "Usage: cucumber-electron"
+    And stdout should include "--interactive"
+    And stdout should include "UNSUPPORTED in cucumber-electron: force shutdown of the event loop"
+    And stdout should include "For more details please visit https://github.com/cucumber/cucumber-js#cli"
