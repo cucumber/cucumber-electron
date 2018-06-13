70ac54e7b149dfc04d1fd3db854c3147f315e0fe
Matt Wynne <matt@mattwynne.net>
2018-06-13 15:43:51 +0100

    Use browsermonkey to interact with DOM

--- examples/angularjs/features/step_definitions/steps.js
+++ examples/angularjs/features/step_definitions/steps.js
@@ -1,4 +1,5 @@
 const assert = require("assert")
+const createBrowser = require('browser-monkey/create')
 
 const {Before, After, When, Then} = require("cucumber")
 
@@ -22,6 +23,7 @@ function angularAppMounted(window) {
 Before(async function () {
   const iframe = await mountIframe(__dirname + "/../../index.html")
   await angularAppMounted(iframe.contentWindow)
+  this.browser = createBrowser(iframe.contentWindow.document)
   this.doc = iframe.contentWindow.document
 })
 
@@ -29,12 +31,10 @@ After(function () {
   // this.app.frameElement.remove()
 })
 
-When("you add the task {string}", function (task) {
-  const input = this.doc.querySelector(".new-todo")
-  input.value = task
-
-  const form = this.doc.querySelector('.todo-form')
-  form.submit()
+When("you add the task {string}", async function (task) {
+  const input = this.browser.find('.new-todo')
+  await input.typeIn(task)
+  await input.submit()
 })
 
 Then("the list of active tasks contains:", function (table) {
